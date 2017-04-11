b9de4763e513e50c224f573add40489db1f32e24
Brian Cortes <brian@monoku.com>
2017-04-11 11:05:09 -0500

    change theme presentation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
   cube = require('bespoke-theme-cube'),
+  nebula = require('bespoke-theme-nebula'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
@@ -12,7 +13,8 @@ var bespoke = require('bespoke'),
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  // cube(),
+  nebula(),
   keys(),
   touch(),
   bullets('li, .bullet'),
