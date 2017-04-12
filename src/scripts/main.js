49696f0b169146c88dbd2f9814c01327e2561ef9
Brian Cortes <brian@monoku.com>
2017-04-12 17:02:40 -0500

    animation  in the team

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -35,3 +35,20 @@ require('prism');
 
 var tl = new gsap.TimelineMax({repeat:-1, repeatDelay:1, yoyo:true});
 tl.staggerTo("h5", 0.2, {className:"+=superShadow", top:"-=9px", ease:Power1.easeIn}, "0.2", "start")
+
+var panel1 = document.querySelector('#panel1'),
+    panel2 = document.querySelector("#panel2"),
+    panel3 = document.querySelector("#panel3"),
+    panel1Text = document.querySelector("#text1"),
+    panel2Text = document.querySelector("#panel2 h2"),
+    info = document.querySelector("#info"),
+    list = document.querySelector("li"),
+    orderNow = document.querySelector("#orderNow"),
+    tl;
+
+var bc = new TimelineMax({delay:0.5, repeat:-1, repeatDelay:2});
+
+//banner animation code (only 11 lines)
+
+ bc.from(panel1, 0.5, {autoAlpha:0})
+   .from(panel1Text, 0.5, {scale:0.5, autoAlpha:0, ease:Back.easeOut});
